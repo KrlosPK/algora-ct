4bcdef2c11b336c7dc5ceb8582badb93c0491c87
ConanGH-S <blandon0207s@gmail.com>
2024-03-13 19:58:40 -0500

    Add - define project colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: 'var(--primary-color)',
+				secondary: 'var(--secondary-color)',
+				base: 'var(--base-color)',
+				foreground: 'var(--foreground-color)'
+			}
+		},
 	},
 	plugins: [],
 }
