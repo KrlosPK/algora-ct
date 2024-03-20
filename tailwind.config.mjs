14af80a9ecdd234e36b76fc8999308d2777d3bde
ConanGH-S <blandon0207s@gmail.com>
2024-03-20 17:20:21 -0500

    Remove css vars and write it down in tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,10 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				primary: 'var(--primary-color)',
-				secondary: 'var(--secondary-color)',
-				'base-primary': 'var(--base-color)',
-				foreground: 'var(--foreground-color)'
+				primary: '#00b3cd',
+				secondary: '#00799c',
+				'base-primary': '#fff',
+				foreground: '#191919'
 			}
 		},
 	},
